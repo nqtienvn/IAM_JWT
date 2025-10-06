924768c27edd55305e85c6fafca2a5701aeb9e52
nqtienvn <quangtiencoder1876@gmail.com>
2025-10-06 13:14:55 +0700

    nothing

---
+++ src/main/java/com/tien/iamservice_jwt/IamServiceJwtApplication.java
@@ -0,0 +1,13 @@
+package com.tien.iamservice_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IamServiceJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IamServiceJwtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tien/iamservice_jwt/IamServiceJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tien.iamservice_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IamServiceJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
